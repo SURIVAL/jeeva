41c066a423145712a15124ad4d188d30be8a057c
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 17:12:28 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+# jeeva
+#include<stdio.h>
+void main()
+{
+int a,b,i,flag;
+printf("enter the two number");
+scanf("%d",&a);
+while(a<b)
+{
+flag=0;
+for(i=2;i<=a/2;++i)
+{
+if(a%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+printf("a", "");
+}
+return0;
+}
