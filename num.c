97191de20a870ccd95586d36f2e8b77812365954
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 16:47:13 +0530

    num.c

---
+++ num.c
@@ -0,0 +1,12 @@
+# jeeva
+#include<stdio.h>
+void main()
+{
+int num,i=20,j=30;
+printf("print odd number in a given range mton:\n);
+for(num=i;num<=j;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+}
