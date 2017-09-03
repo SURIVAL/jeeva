ae0043f839a4678fe1e514bf8826eace6bfc7a3a
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 17:03:14 +0530

    interval.c

---
+++ interval.c
@@ -0,0 +1,12 @@
+# jeeva
+#include<stdio.h>
+void main()
+{
+int num,a=30,b=35;
+printf("print even number in a given number in a given range atob :\b);
+for(num=a;num<=b;num++)
+{
+if(num%2==1)
+print("%d",num);
+}
+}
